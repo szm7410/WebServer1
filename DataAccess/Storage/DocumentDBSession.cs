cc6a55bd775741d2c4372ca2f0388241d44c5715
Yuchen Meng <v-yumen@microsoft.com>
2017-02-21 15:31:08 +0800

    add document query session

---
+++ DataAccess/Storage/DocumentDBSession.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+
+namespace DataAccess.Storage
+{
+    class DocumentDBSession
+    {
+        private Uri uri;
+        private string authorizationKey;
+        private DocumentClient dc;
+
+        public DocumentDBSession(string uri, string authorizationKey)
+        {
+            this.uri = new Uri(uri);
+            this.authorizationKey = authorizationKey;
+            dc = new DocumentClient(this.uri, this.authorizationKey);
+        }
+
+        public IEnumerable<dynamic> QueryItem(string databaseid, string collectionid, string sql, FeedOptions fo)
+        {
+            IQueryable<dynamic> query = dc.CreateDocumentQuery<dynamic>(UriFactory.CreateDocumentCollectionUri(databaseid, collectionid), sql, fo);
+            return query.AsEnumerable<dynamic>();
+        }
+    }
+}
